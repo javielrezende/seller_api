7e298c4afa1483084ac2c7e23c48c48d88f567af
Roger Rezende <javielrezende@gmail.com>
2021-04-28 23:02:41 -0300

    :sparkles: Add seller controller
    Add seller controller

---
+++ app/Http/Controllers/SellerController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Repositories\Seller\SellerRepositoryContract;
+use App\Http\Requests\StoreSeller;
+use App\Http\Services\Seller\SellerServiceContract;
+use App\Models\Seller;
+use App\Traits\LogTrait;
+use Exception;
+use Illuminate\Http\JsonResponse;
+
+class SellerController extends Controller
+{
+    use LogTrait;
+
+    private $sellerRepository;
+
+    /**
+     * __construct
+     *
+     * @param  SellerRepositoryContract $sellerRepository
+     */
+    public function __construct(SellerRepositoryContract $sellerRepository)
+    {
+        $this->sellerRepository = $sellerRepository;
+    }
+
+    /**
+     * List Sellers
+     *
+     * @return JsonResponse
+     */
+    public function list()
+    {
+        try {
+            $sellers = $this->sellerRepository->all();
+
+            return new JsonResponse(['data' => $sellers], 200);
+        } catch (Exception $exception) {
+            $this->criticalLog('Failed to list sellers', SellerController::class, $exception->getMessage());
+
+            return new JsonResponse([
+                'code' => 'seller_list_error',
+                'message' => 'Erro ao listar vendedores'
+            ], 500);
+        }
+    }
+
+    /**
+     * Store a Seller
+     *
+     * @param  StoreSeller $request
+     * @return void
+     */
+    public function store(StoreSeller $request)
+    {
+        try {
+            $data = [
+                'name' => $request->name,
+                'email' => $request->email
+            ];
+
+            $this->sellerRepository->create($data);
+
+            return new JsonResponse([
+                'code' => 'seller_created',
+                'message' => 'Vendedor cadastrado'
+                ], 201);
+        } catch (Exception $exception) {
+            $this->criticalLog('Failed to create a seller', SellerController::class, $exception->getMessage());
+
+            return new JsonResponse([
+                'code' => 'seller_creation_error',
+                'message' => 'Erro ao cadastrar um vendedor'
+            ], 500);
+        }
+    }
+}
