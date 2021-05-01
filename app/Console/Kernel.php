7f121823f1411ea577f44948ba514341ec6617d0
Roger Rezende <javielrezende@gmail.com>
2021-05-01 00:46:16 -0300

    :sparkles: Scheduling report for the end of the night
    Scheduling report for the end of the night

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\DailySalesReport;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command(DailySalesReport::class)->timezone('America/Sao_Paulo')->at('20:30');
     }
 
     /**
